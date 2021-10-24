a919ed0dcfa4eca0f848784c06a59b5b84180dc0
Ilona Yuvchenko <ilona.yuv@gmail.com>
2021-10-24 22:18:05 +0300

    Create vars.js
    Синтаксис (const и let, console.log(), тип переменной
    const type = typeof 5;
    console.log(type);  alert)

---
+++ vars.js
@@ -0,0 +1,20 @@
+const age = 10;
+const totalPrice = 200.74;
+const userName = "Juventus";
+const message = "Добро пожаловать!";
+const isOpen = true;
+const shouldConfirm = false;
+
+let x = 5;
+x = 10;
+
+console.log("Цена: ", totalPrice);
+
+const type = typeof "ilona";
+
+console.log(type);
+
+//console.log("до");
+//alert("lolololo");
+//console.log("после");
+
