6ce741aec9561e57f4ae8be4e3181d0774e1a631
Ilona Yuvchenko <ilona.yuv@gmail.com>
2021-10-24 22:18:42 +0300

    Create input.js
    comfirm()
    prompt()

---
+++ input.js
@@ -0,0 +1,7 @@
+const shouldRenew = confirm("Хотите подтвердить подписку?");
+console.log(shouldRenew);
+
+let quantity = prompt("Введите количество товаров");
+quantity = Number(quantity);
+console.log(quantity);
+console.log(typeof quantity);
