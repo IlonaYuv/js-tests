79154df7f736d34975747aa777054f23f263334e
Ilona Yuvchenko <ilona.yuv@gmail.com>
2021-11-03 00:36:26 +0200

    Update change-register.js

--- change-register.js
+++ change-register.js
@@ -4,3 +4,31 @@
 "jAVAsCRIPT"*/
 
 const string = 'JavaScript';
+const letters = string.split(''); 
+let invertedString;
+
+console.log(letters);
+
+for (const letter of letters) {
+    console.log(letter);
+
+    //if (letter === letter.toLowerCase()) {
+    //    console.log('Эта буква в нижнем регистре!!! - ', letter);
+
+    //    invertedString += letter.toUpperCase();
+    //} else {
+    //    console.log('Эта буква в верхнем регистре!!! - ', letter);
+    //     invertedString += letter.toLowerCase();
+    //}
+
+    //invertedString +=
+    //    letter === letter.toLowerCase()
+    //    ? letter.toUpperCase()
+    //    : letter.toLowerCase();
+    
+    const isEqual = letter === letter.toLowerCase();
+    invertedString += isEqual ? letter.toUpperCase() : letter.toLocaleLowerCase();
+}
+
+
+console.log('invertedString: ', invertedString);
