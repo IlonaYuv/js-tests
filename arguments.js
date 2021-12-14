6ac40b55cba306722d38f97d6a1d1202b89c8b5e
Ilona Yuvchenko <ilona.yuv@gmail.com>
2021-12-15 01:32:53 +0200

    Update arguments.js

--- arguments.js
+++ arguments.js
@@ -28,17 +28,17 @@
 /* Напиши функцию add для сложения производительного к-ства аргументов (чисел)
 - Операция ... (rest) */
 
-const add = function (...args) {
-    console.log(args);
+//const add = function (...args) {
+//    console.log(args);
 
-    let total = 0;
+//    let total = 0;
 
-    for (const arg of args) {
-      total += arg;
-    }
+//    for (const arg of args) {
+//      total += arg;
+//    }
        
-    return total;
-};
+//    return total;
+//};
 
 // console.log(add(1, 2, 3));
 // console.log(add(1, 2, 4, 5, 6));
@@ -51,22 +51,28 @@ const add = function (...args) {
 начиная со второго,
 - для которых есть аналог в оригинальном масссиве */
 
-const filterNumbers = function (array, ...args) {
-    console.log('array: ', array);
-    console.log('args: ', args);
-    const uniqueElements = [];
+//const filterNumbers = function (array, ...args) {
+//   console.log('array: ', array);
+//    console.log('args: ', args);
+//    const uniqueElements = [];
 
-    for (const element of array) {
-        if (args.includes(element)) {
-            uniqueElements.push(element);
+//    for (const element of array) {
+//        if (args.includes(element)) {
+//            uniqueElements.push(element);
 
-            console.log(`${element} есть везде!`);  
-        } 
-    }
+//            console.log(`${element} есть везде!`);  
+//        } 
+//    }
+//
+//    return uniqueElements;  
+// };
 
-    return uniqueElements;  
- };
+// console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); //[2, 3]
+// console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); //[30, 15]
+// console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); //[200]
 
- console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); //[2, 3]
- console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); //[30, 15]
- console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); //[200]
+
+
+
+
+
